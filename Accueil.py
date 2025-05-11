055afb631212239df5bdbd655f16791632050613
Hamza Bakka <49447582+8aks@users.noreply.github.com>
2025-05-11 20:17:28 +0200

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -8,7 +8,7 @@ st.write("Bienvenue ! Choisissez ce que vous souhaitez explorer :")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.page_link("pages/1_📄_Recherche_sur_ArXiv.py", label="🔍 Rechercher des articles scientifiques", icon="📄")
+    st.page_link("pages/Recherche_sur_ArXiv.py", label="🔍 Rechercher des articles scientifiques", icon="📄")
 
 with col2:
-    st.page_link("pages/2_🐶_Infos_Race_de_Chien.py", label="🐶 Informations sur une race de chien", icon="🐶")
+    st.page_link("pages/Infos_Race_de_Chien.py", label="🐶 Informations sur une race de chien", icon="🐶")
